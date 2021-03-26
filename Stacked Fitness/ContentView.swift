87e9c822008c0146957e51d2ea49f95cf571768e
Andrei Istoc <andreistoc@gmail.com>
2021-03-26 17:22:05 +0200

    Initial Commit

---
+++ Stacked Fitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stacked Fitness
+//
+//  Created by Andrei Istoc on 26.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stacked Fitness/Stacked_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacked_FitnessApp.swift
+//  Stacked Fitness
+//
+//  Created by Andrei Istoc on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Stacked_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
